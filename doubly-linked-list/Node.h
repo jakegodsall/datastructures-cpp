65dcd0e926c51564f1bcb2a731a3285c2d7e810c
Jake Godsall <jake.edward.godsall@gmail.com>
2023-08-21 14:40:18 +0100

    added getter and setters for all members

--- doubly-linked-list/Node.h
+++ doubly-linked-list/Node.h
@@ -6,6 +6,14 @@ class Node
     public:
         Node(int data);
 
+        int getData() const;
+        Node* getPrev() const;
+        Node* getNext() const;
+
+        void setData(int data);
+        void setPrev(Node* prev);
+        void setNext(Node* next);
+
     private:
         int data;
         Node* prev;
