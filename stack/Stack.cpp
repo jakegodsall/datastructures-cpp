7b8aaf442d84b66fa6cc18593f953629fcd7cbdc
jakegodsall <jake.edward.godsall@gmail.com>
2023-08-25 14:07:09 +0200

    Added explanatory comments to Stack implementation (and node)

--- stack/Stack.cpp
+++ stack/Stack.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "Stack.h"
 
+// Default constructor: Initializes an empty stack.
 Stack::Stack()
 {
     this->top = nullptr;
     this->height = 0;
 }
 
+// Constructor: Initializes the stack with a single node.
 Stack::Stack(int value)
 {
     StackNode* newNode = new StackNode(value);
@@ -14,26 +16,31 @@ Stack::Stack(int value)
     this->height = 1;
 }
 
+// Returns the top node of the stack.
 StackNode* Stack::getTop() const
 {
     return this->top;
 }
 
+// Returns the height (number of nodes) in the stack.
 int Stack::getHeight() const
 {
     return this->height;
 }
 
+// Sets the top node of the stack.
 void Stack::setTop(StackNode* stackNode)
 {
     this->top = stackNode;
 }
 
+// Sets the height (number of nodes) of the stack.
 void Stack::setHeight(int height)
 {
     this->height = height;
 }
 
+// Prints the values of the nodes in the stack from top to bottom.
 void Stack::printStack() const
 {
     StackNode* temp = this->top;
@@ -45,10 +52,11 @@ void Stack::printStack() const
     }
 }
 
+// Adds a new node with the specified value to the top of the stack.
 void Stack::push(int value)
 {
-    StackNode* newNode = new StackNode(value);
-    newNode->setNext(this->top);
-    this->top = newNode;
-    this->height++;
+    StackNode* newNode = new StackNode(value); // Create the new node.
+    newNode->setNext(this->top); // Set the new node to point to the node currently at the top of the stack.
+    this->top = newNode; // Set the top pointer to point to the new node.
+    this->height++; // Increase the height of the stack by 1.
 }
--- stack/StackNode.cpp
+++ stack/StackNode.cpp
@@ -1,27 +1,32 @@
 #include "StackNode.h"
 
+// Constructor: Initializes the StackNode with a given value and sets the next pointer to nullptr.
 StackNode::StackNode(int value)
 {
-    this->value = value;
-    this->next = nullptr;
+    this->value = value;  // Assign the passed value to the node's value attribute.
+    this->next = nullptr; // The next pointer is set to nullptr since we're creating a new node.
 }
 
+// Getter method: Returns the value of the StackNode.
 int StackNode::getValue() const
 {
-    return this->value;
+    return this->value;   // Return the value stored in the node.
 }
 
+// Getter method: Returns the pointer to the next StackNode in the list (or nullptr if this is the last node).
 StackNode* StackNode::getNext() const
 {
-    return this->next;
+    return this->next;   // Return the pointer to the next node.
 }
 
+// Setter method: Sets the value of the StackNode.
 void StackNode::setValue(int value)
 {
-    this->value = value;
+    this->value = value; // Assign the passed value to the node's value attribute.
 }
 
+// Setter method: Sets the pointer to the next StackNode in the list.
 void StackNode::setNext(StackNode* next)
 {
-    this->next = next;
+    this->next = next;   // Assign the passed pointer to the node's next attribute.
 }
