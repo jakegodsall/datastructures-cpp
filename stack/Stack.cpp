70b02093657e9382c7f55bcd22b518c4f42ba120
jakegodsall <jake.edward.godsall@gmail.com>
2023-08-25 13:07:38 +0200

    Built basic shell of Stack with constructors and getters/setters.

--- stack/Stack.cpp
+++ stack/Stack.cpp
@@ -1,1 +1,34 @@
 #include "Stack.h"
+
+Stack::Stack()
+{
+    this->top = nullptr;
+    this->height = 0;
+}
+
+Stack::Stack(int value)
+{
+    StackNode* newNode = new StackNode(value);
+    this->top = newNode;
+    this->height = 1;
+}
+
+StackNode* Stack::getTop() const
+{
+    return this->top;
+}
+
+int Stack::getHeight() const
+{
+    return this->height;
+}
+
+void Stack::setTop(StackNode* stackNode)
+{
+    this->top = stackNode;
+}
+
+void Stack::setHeight(int height)
+{
+    this->height = height;
+}
--- stack/Stack.h
+++ stack/Stack.h
@@ -1,11 +1,23 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include "StackNode.h"
+
 class Stack
 {
 public:
+    Stack();
+    Stack(int value);
+
+    StackNode* getTop() const;
+    int getHeight() const;
+
+    void setTop(StackNode* stackNode);
+    void setHeight(int height);
 
 private:
+    StackNode* top;
+    int height;
 };
 
 #endif
